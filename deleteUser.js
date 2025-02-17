02898a006ccea7786817b125354573f016fe9827
Rizky Eka Pratama <rizkyekap96@gmail.com>
2025-02-17 11:49:02 +0700

    delete user

--- deleteUser.js
+++ deleteUser.js
@@ -5,7 +5,6 @@ describe('Delete User', async function() {
         this.timeout(5000);
         const url = 'https://reqres.in/api/users/1';
 
-        // Send a DELETE request to the API
         const response = await fetch(url, {
             method: "DELETE",
             headers: {
