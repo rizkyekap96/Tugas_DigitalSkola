08afe9b770e65f17aa43d0bc915edde492778ced
Rizky Eka Pratama <rizkyekap96@gmail.com>
2025-02-17 11:07:31 +0700

    create user

---
+++ createUser.js
@@ -0,0 +1,28 @@
+const assert = require("assert");
+
+describe('Create User', async function(){
+    it('Create user should be success', async function (){
+        this.timeout(5000)
+        const url ='https://reqres.in/api/users'
+        const requestData = {
+            "name":"Rizky Eka Pratama",
+            "job":"Digital Skola Batch 8"
+        };
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(requestData)
+        });
+
+        const Responsedata = await response.json();
+
+        console.log("Response : " + JSON.stringify(Responsedata, null, 1));
+
+        assert.strictEqual(response.status, 201, "Status harus 201");
+
+        assert.strictEqual(Responsedata.name, "Rizky Eka Pratama");
+    })
+})
